06379b14c102c8bea0dff53aa08b4ebfd54553b1
杨宇航 <y17770372709@outlook.com>
2026-08-16 07:59:47 +0800

    refactor: remove unused runtime reexports

--- hiad/runtime/__init__.py
+++ hiad/runtime/__init__.py
@@ -1,13 +1,1 @@
 """Shared detector execution primitives."""
-
-from .evidence import (
-    denormalize_imagenet_batch,
-    fuse_evidence_tensors,
-    high_frequency_map,
-)
-
-__all__ = [
-    "fuse_evidence_tensors",
-    "denormalize_imagenet_batch",
-    "high_frequency_map",
-]
